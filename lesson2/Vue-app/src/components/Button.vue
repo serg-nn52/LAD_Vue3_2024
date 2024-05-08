d40cb3919278bd8c75f09eab520b2748ce190af5
Sergey Lushkin <serg.nn1985@gmail.com>
2024-05-08 21:36:16 +0300

    lesson2

---
+++ lesson2/Vue-app/src/components/Button.vue
@@ -0,0 +1,25 @@
+<template>
+  <button @click="$emit('clickMe', count)">{{ buttonName + ' ' + count }}</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    buttonName: {
+      type: String,
+      require: true,
+      default: 'Counter',
+    },
+    count: {
+      type: Number,
+      require: true,
+      default: 0,
+    },
+  },
+  emits: ['clickMe'],
+});
+</script>
+
+<style scoped></style>
---
+++ lesson2/Vue-app/src/components/Header.vue
@@ -0,0 +1,61 @@
+<template>
+  <header v-bind:style="styles" class="header">Header {{ count }}</header>
+  <HeaderButton
+    @click-me="(value) => listenerButton(value)"
+    :count="count"
+    :button-name="'NewCounter'"
+    @click="incrementCount"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Button from './Button.vue';
+
+export default defineComponent({
+  components: {
+    HeaderButton: Button,
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    incrementCount() {
+      this.count++;
+    },
+    listenerButton(value: number) {
+      console.log(value);
+    },
+  },
+  computed: {
+    isEven() {
+      console.log('computed');
+      return !(this.count % 2);
+    },
+    styles() {
+      return { backgroundColor: this.isEven ? 'grey' : 'green' };
+    },
+  },
+  watch: {
+    count(newValue, oldValue) {
+      console.log('newValue', newValue);
+      console.log('oldValue', oldValue);
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.header {
+  height: 150px;
+  background-color: grey;
+  color: black;
+  font-size: 24px;
+  font-weight: 900;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
